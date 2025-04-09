6abc26f3de84a17b4235b09c4075e37d64a525ea
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2025-04-09 02:49:25 -0500

    tab system plus web digest init

--- src/content.js
+++ src/content.js
@@ -1,309 +1,3 @@
-// import showdown from './lib/showdown-2.1.0/dist/showdown.min.js'
-
-const sidebarHTML = `
-    <div>
-        <div class="sidebar-header">
-            <button id="close-sidebar">\></button>
-            <button id="info-button" onClick="window.open('https://github.com/chad-russell-git/chrome-ollama-extension/', '_blank');">i</button>
-            <h2>Page Summarizer</h2>
-        </div>
-        <div class="settings">
-            <select id="input-source">
-                <option value="web-page">From Web Page</option>
-                <option value="clipboard">From Clipboard</option>
-            </select>
-            <div class="slider-container"> 
-                <h3>Response Length</h3>
-                <input type="range" id="verbose-slider" min="0" max="100" value="50">
-            </div>
-        </div>
-        <div class="actions">
-            <button id="summarize-button">Summarize</button>
-            <div id="status-circle" class="ollama-status-div">
-                <img id="ollama-status" src="chrome-extension://${chrome.runtime.id}/src/img/ollama.png" alt="Ollama Logo" style="width: 20px; height: 20px;">
-                <img id="ollama-status-loading" src="chrome-extension://${chrome.runtime.id}/src/img/cloud.gif" alt="Loading GIF">   
-            </div>
-        </div>
-        <div id="summary-output"></div>
-
-        <form id="ollama-chat-form" class="page-chat">
-            <input type="text" id="ollama-chat-input" placeholder="Chat with the page..." />
-            <button type="submit" id="ollama-chat-submit">Send</button>
-        </form>
-
-    </div>
-`;
-
-const sidebarCSS = `
-    #ollama-sidebar {
-        background-color:rgb(0, 0, 0);
-        background-image: linear-gradient(to bottom, rgb(0, 0, 0), rgb(46, 46, 46));
-        padding: 10px;
-        border-bottom: 1px solid #ccc;
-        text-align: center;
-        position: fixed;
-        top: 0;
-        right: 0;
-        width: 30%;
-        min-width: 300px;
-        max-width: 600px;
-        height: 100%;
-        z-index: 9999;
-        box-shadow: -2px 0 5px rgba(0,0,0,0.5);
-    }
-
-    #ollama-sidebar .page-chat {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-top: 10px;
-    }
-
-    #ollama-sidebar .page-chat input {
-        width: 80%;
-        padding: 5px;
-        border: 1px solid #ccc;
-        font-size: 14px;
-        font-family: 'Open Sans', sans-serif;
-    }
-
-    #ollama-sidebar .page-chat button {
-        background-color: #76b900;
-        width: 20%;
-        color: black;
-        border: none;
-        padding: 6px 10px;
-        padding-bottom: 5px;
-        cursor: pointer;
-        font-size: 14px;
-        font-family: 'Open Sans', sans-serif;
-        font-weight: bold;
-    }
-
-    #ollama-sidebar #close-sidebar {
-        background-color: rgba(0,0,0,0);
-        position: absolute;
-        left: 3px;
-        top: 3px;
-        color: white;
-        border: none;
-        cursor: pointer;
-        font-size: 15px;
-        font-weight: bold;
-        width: 20px;
-        height: 20px;
-        font-family: 'Open Sans', sans-serif;
-        font-weight: bold;
-        border-radius: 5px;
-        text-align: center;
-    }
-
-    #ollama-sidebar #close-sidebar img {
-        transform: translate(-5px, 0px);
-    }
-
-    
-    #ollama-sidebar #info-button {
-        background-color:rgb(0, 0, 0);
-        color: #76b900;
-        border: 1px solid #76b900 !important;
-        position: absolute;
-        right: 5px;
-        top: 5px;
-        border: none;
-        cursor: pointer;
-        font-size: 9px;
-        font-weight: bold;
-        width: 16px;
-        height: 16px;
-        font-family: "Open Sans", sans-serif;
-        font-weight: bold;
-        border-radius: 50%;
-        text-align: center;
-    }
-    
-    #ollama-sidebar .settings {
-        width: 100%;
-        display: inline-flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-top: 0;
-    }
-        
-    #ollama-sidebar .settings * {
-        width: 100%;
-        align-items: center;
-        margin: 0px 5px;
-        margin-top: -10px;
-    }
-
-    #ollama-sidebar select {
-        background-color: #76b900;
-        color: black;
-        border: none;
-        margin: 5px;
-        cursor: pointer;
-        padding: 5px 0px;
-        font-size: 12px;
-        font-family: 'Open Sans', sans-serif;
-        width: 30%;
-    }
-
-    #ollama-sidebar #verbose-slider {
-        width: 100%;
-        accent-color: #76b900;
-    }
-
-    #ollama-sidebar .actions {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-    }
-
-    #ollama-sidebar .ollama-status-div {
-        background-color: #aaa;
-        border-radius: 50%;
-        width: 30px;
-        height: 30px;
-        margin-right: 10px;
-    }
-
-    #ollama-sidebar .actions #ollama-status {
-        transition: background-color 0.3s ease;
-        transform: translate(0px, 3px);
-    }
-
-    #ollama-sidebar .actions #ollama-status-loading {
-        display: block;
-        width: 20px;
-        height: 20px;
-        position: absolute;
-        transform: translate(20px, -34px);
-    }
-        
-    #ollama-sidebar #summarize-button {
-        background-color: #76b900;
-        color: black;
-        font-weight: bold;
-        border: none;
-        padding: 4px 40px;
-        width: calc(50% - 10px);
-        margin: 5px;
-        cursor: pointer;
-        font-size: 16px;
-        margin-top: 5px;
-        font-family: 'Open Sans', sans-serif;
-        align-self: left;
-    }
-
-    #ollama-sidebar h2 {
-        color: #fff;
-        font-family: 'Open Sans', sans-serif;
-        font-size: 16px;
-        font-weight: bold;
-        margin-bottom: 10px;
-        margin-top: -5px;
-    }
-
-    #ollama-sidebar h3 {
-        color: #fff;
-        font-family: 'Open Sans', sans-serif;
-        font-size: 12px;
-        align: left;
-        margin-top: 0px;
-        margin-bottom: 0px;
-        text-align: left;
-        padding-top: 10px;
-    }
-
-
-    #ollama-sidebar #summary-output {
-        background-color: #fff;
-        border: 1px solid #ccc;
-        padding: 10px;
-        margin-top: 10px;
-        min-height: 60vh;
-        max-height: 60vh;
-        overflow-y: auto;
-        font-family: 'Arial', sans-serif;
-        font-size: 11px;
-        text-align: left;
-        color: black;
-    }
-
-    #ollama-sidebar #summary-output code {
-        padding: 2px 5px;
-        border-radius: 3px;
-        font-family: 'Courier New', Courier, monospace;
-        font-size: 12px;
-        color: #333;
-        text-shadow: none;
-    }
-
-    #ollama-sidebar #summary-output h1 {
-        ont-family: 'Arial', sans-serif;
-        font-size: 24px;
-        font-weight: bold;
-        margin: 10px 0;
-        color: #000;
-    }
-
-    #ollama-sidebar #summary-output h2 {
-        font-family: 'Arial', sans-serif;
-        font-size: 20px;
-        font-weight: bold;
-        margin: 8px 0;
-        color: #000;
-    }
-
-    #ollama-sidebar #summary-output h3 {
-        font-family: 'Arial', sans-serif;
-        font-size: 18px;
-        font-weight: bold;
-        margin: 6px 0;
-        color: #000;
-    }
-
-    #ollama-sidebar #summary-output h4 {
-        font-family: 'Arial', sans-serif;
-        font-size: 16px;
-        font-weight: bold;
-        margin: 4px 0;
-        color: #000;
-    }
-
-    #ollama-sidebar #summary-output p {
-        font-family: 'Arial', sans-serif;
-        font-size: 14px;
-        line-height: 1.5;
-        margin: 8px 0;
-        color: #000;
-    }
-
-    #ollama-sidebar #summary-output ul {
-        font-family: 'Arial', sans-serif;
-        margin: 10px 0;
-        padding-left: 20px;
-        list-style-type: disc;
-    }
-
-    #ollama-sidebar #summary-output ol {
-        font-family: 'Arial', sans-serif;
-        margin: 10px 0;
-        padding-left: 20px;
-        list-style-type: decimal;
-    }
-
-    #ollama-sidebar #summary-output li {
-        font-family: 'Arial', sans-serif;
-        font-size: 14px;
-        line-height: 1.5;
-        margin: 4px 0;
-        color: #000;
-    }
-`;
-
 const convertedContent = async (content) => {
     const converter = new showdown.Converter();
     const html = converter.makeHtml(content);
@@ -312,25 +6,40 @@ const convertedContent = async (content) => {
 
 statusInterval = null;
 
-const injectSidebar = () => {
+const injectSidebar = async () => {
     // Check if the sidebar already exists
     const existingSidebar = document.getElementById('ollama-sidebar');
     if (existingSidebar) {
-        existingSidebar.remove(); // Remove the existing sidebar
+        existingSidebar.remove();
     }
 
-    // Create a sidebar element
+    // Create a sidebar element and inject html
     const sidebar = document.createElement('div');
     sidebar.id = 'ollama-sidebar';
-    sidebar.innerHTML = sidebarHTML;
+    await fetch("chrome-extension://" + chrome.runtime.id + "/src/sidebar/sidebar.html")
+        .then(response => response.text())
+        .then(sidebarhtml => {
+            sidebarhtml = sidebarhtml.replaceAll("${chrome.runtime.id}", chrome.runtime.id);
+            sidebar.innerHTML = sidebarhtml;
+        })
+        .catch(error => {
+            console.error('Error fetching sidebar HTML:', error);
+        });
     document.body.appendChild(sidebar);
     
-    // Create a style element and append the CSS to it
+    // Create a style element and inject css
     const style = document.createElement('style');
-    style.textContent = sidebarCSS;
+    await fetch("chrome-extension://" + chrome.runtime.id + "/src/sidebar/sidebar.css")
+        .then(response => response.text())
+        .then(sidebarcss => {
+            style.textContent = sidebarcss;
+        })
+        .catch(error => {
+            console.error('Error fetching sidebar CSS:', error);
+        });
     document.head.appendChild(style);
 
-    //  Load the Google Fonts stylesheet
+    //  Load the Google Fonts stylesheet for OPEN SANS
     const link = document.createElement('link');
     link.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap';
     link.rel = 'stylesheet';
@@ -342,29 +51,44 @@ const injectSidebar = () => {
         sidebar.remove(); // Remove the sidebar from the DOM
     });
 
-    // Add event listener to the summarize button
+    // Add event listener to the tab buttons
+    const pageTabButton = document.getElementById('summary-toggle');
+    const chatTabButton = document.getElementById('web-toggle');
+    const pageTabContent = document.getElementById('summary-tab');
+    const chatTabContent = document.getElementById('digest-tab');
+    pageTabButton.addEventListener('click', () => {
+        console.log('Page tab clicked');
+        pageTabContent.style.display = 'block';
+        chatTabContent.style.display = 'none';
+    });
+    chatTabButton.addEventListener('click', () => {
+        console.log('Chat tab clicked');
+        chatTabContent.style.display = 'block';
+        pageTabContent.style.display = 'none';
+    });
+
+    // Add event listener to the summarize button (handle content selection)
     const summarizeButton = document.getElementById('summarize-button');
     summarizeButton.addEventListener('click', () => {
-        //get the verbose level from the slider
         const verboseSlider = document.getElementById('verbose-slider');
         const verboseLevel = verboseSlider.value;
 
-        // get the input source from the select element
         const inputSource = document.getElementById('input-source').value;
-        console.log('Input source:', inputSource); // Debug log
+        console.log('Input source:', inputSource);
+
         if (inputSource === 'clipboard') {
             navigator.clipboard.readText().then((text) => {
                 fetchSummary(text, verboseLevel);
             }).catch((error) => {
-                console.error('Failed to read clipboard:', error); // Debug log
+                console.error('Failed to read clipboard:', error);
             });
         } else if (inputSource === 'web-page') {
-            // If the input source is the web page, fetch the content from the body
-            console.log('Fetching content from web page...'); // Debug log
+            console.log('Fetching content from web page...');
             fetchSummary(document.body.innerText || '', verboseLevel);
         }
     });
-
+    
+    // Add event listener to the chat-with-page form (handle chat input)
     const pageChatForm = document.getElementById('ollama-chat-form');
     pageChatForm.addEventListener('submit', (event) => {
         event.preventDefault(); // Prevent the form from submitting
@@ -376,9 +100,22 @@ const injectSidebar = () => {
         if (userInput) {
             pageContent = document.body.innerText || '';
 
-            console.log("Length of page content: ", pageContent.length); // Debug log
+            console.log("Length of page content: ", pageContent.length);
             fetchPageChat(pageContent, userInput, verboseLevel);
             chatInput.value = ''; // Clear the input field after sending
+        }
+    });
+
+    // Add event listener to the web digest form (handle web digest input)
+    const webDigestForm = document.getElementById('digest-form');
+    webDigestForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // Prevent the form from submitting
+        const digestInput = document.getElementById('digest-input');
+        const query = digestInput.value;
+
+        if (query) {
+            fetchWebDigest(query);
+            digestInput.value = ''; // Clear the input field after sending
         }
     });
 
@@ -421,6 +158,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 });
 
+
+
+
+
 async function fetchSummary(content, verboseLevel) {
     console.log('fetchSummary called with web content.'); // Debug log
     console.log('fetchSummary called with verboseLevel: ', verboseLevel); // Debug log
@@ -436,7 +177,7 @@ async function fetchSummary(content, verboseLevel) {
         const response = await fetch(ollamaUrl, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 model: 'llama3.2',
@@ -520,6 +261,9 @@ async function fetchSummary(content, verboseLevel) {
         summaryOutput.textContent = 'Error: ' + error.message;
     }
 }
+
+
+
 
 async function fetchPageChat(content, userInput, verboseLevel) {
     console.log('fetchPageChat called with web content.'); // Debug log
@@ -630,4 +374,18 @@ async function fetchPageChat(content, userInput, verboseLevel) {
         console.error('Error in fetchSummary:', error.message);
         summaryOutput.textContent = 'Error: ' + error.message;
     }
+}
+
+fetchWebDigest = async (query) => {
+    console.log('fetchWebDigest called with query:', query);
+    
+    await fetch("https://api.duckduckgo.com/?q=" + query + "&format=json&redirected=1") // LOOK AT https://langsearch.com/overview
+        .then(response => response.text())
+        .then(data => {
+            const jsonData = JSON.parse(data);
+            console.log(jsonData);
+            const outputDiv = document.getElementById('digest-output');
+            outputDiv.innerText = data; // Clear previous output
+        })
+
 }
